d94f6731af6565b69ffa3e5b4635b7f7b2542e01
nixx quality <nixx@is-fantabulo.us>
2015-02-25 16:52:47 +0100

    Initial commit

---
+++ Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace GlobalizationExtras
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Registering ja-JP-Latn...");
+            try
+            {
+                var builder = new CultureAndRegionInfoBuilder("ja-JP-Latn", CultureAndRegionModifiers.None);
+                builder.LoadDataFromCultureInfo(new CultureInfo("ja-JP"));
+                builder.LoadDataFromRegionInfo(new RegionInfo("JP"));
+
+                builder.CultureEnglishName = "Japanese (Romaji)";
+                builder.CultureNativeName = "日本語 (ローマ字)";
+
+                builder.Register();
+                Console.WriteLine("Done!");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Already registered.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Not authorized! Run the program as administrator.");
+            }
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
